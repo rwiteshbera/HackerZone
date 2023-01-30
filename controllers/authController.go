2c7ada6e6a8a83d073cfdf16dbe9edaf0eef964d
Rwitesh Bera <rwiteshbera@gmail.com>
2023-01-30 12:29:15 +0530

    updated signup

--- controllers/authController.go
+++ controllers/authController.go
@@ -1,7 +1,9 @@
 package controllers
 
 import (
+	"database/sql"
 	_ "database/sql"
+	"errors"
 	"github.com/gin-gonic/gin"
 	"github.com/rwiteshbera/HackZone/api"
 	"github.com/rwiteshbera/HackZone/database"
@@ -24,32 +26,29 @@ func SignUp(server *api.Server) gin.HandlerFunc {
 			return
 		}
 
-		// Split full name into firstname and lastname
-		name := strings.Split(signUpRequest.FullName, " ")
-
-		hashedPassword, err1 := utils.HashPassword(signUpRequest.Password)
-		if err1 != nil {
-			logErrorWithAbort(context, err1, http.StatusInternalServerError)
-			return
-		}
-
-		createdAt, _ := time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
-		lastLogin := createdAt
-
 		// Connect with Database
 		db, err := database.Connect(server)
 		if err != nil {
 			logErrorWithAbort(context, err, http.StatusInternalServerError)
 			return
 		}
+		defer db.Close()
 
-		res, err := db.Exec(database.SignUpQuery, signUpRequest.Email, name[0], name[1], hashedPassword, createdAt, lastLogin)
+		// Check if user with the requested email already exists
+		if CheckIfUserExists(db, signUpRequest) {
+			logErrorWithAbort(context, errors.New("user already exists"), http.StatusInternalServerError)
+			return
+		}
+
+		// If email doesn't exist, then add user data
+		err = AddUserData(db, signUpRequest)
 		if err != nil {
 			logErrorWithAbort(context, err, http.StatusInternalServerError)
 			return
 		}
 
-		context.JSON(http.StatusOK, gin.H{"message": res})
+		context.JSON(http.StatusOK, gin.H{"message": "registration successful"})
+
 	}
 }
 
@@ -57,6 +56,37 @@ func Login(server *api.Server) gin.HandlerFunc {
 	return func(context *gin.Context) {
 
 	}
+}
+
+// Check If user with this email already exists or not
+func CheckIfUserExists(db *sql.DB, request models.SignupUserRequest) bool {
+	var email string
+	db.QueryRow(database.CheckIfUserAlreadyExistsQuery, request.Email).Scan(&email)
+	if email == "" {
+		return false
+	}
+	return true
+}
+
+// Add user data to database during signup
+func AddUserData(db *sql.DB, request models.SignupUserRequest) error {
+
+	// Split full name into firstname and lastname
+	name := strings.Split(request.FullName, " ")
+
+	hashedPassword, err := utils.HashPassword(request.Password)
+	if err != nil {
+		return err
+	}
+
+	createdAt, _ := time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+	lastLogin := createdAt
+
+	_, err = db.Exec(database.SignUpQuery, request.Email, name[0], name[1], hashedPassword, createdAt, lastLogin)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 // Handle error
@@ -67,5 +97,4 @@ func logErrorWithAbort(context *gin.Context, err error, statusCode int) {
 // log a message
 func logMessage(context *gin.Context, message any) {
 	context.JSON(http.StatusOK, gin.H{"message": message})
-	return
 }
