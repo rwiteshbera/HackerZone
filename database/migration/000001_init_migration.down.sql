46543a544e42bba3f14f87d06a0c005b66b83706
Rwitesh Bera <rwiteshbera@gmail.com>
2023-02-09 22:28:01 +0530

    formatted database

--- database/migration/000001_init_migration.down.sql
+++ database/migration/000001_init_migration.down.sql
@@ -1,9 +1,1 @@
-CREATE TABLE organizers
-(
-    email     VARCHAR(256) PRIMARY KEY UNIQUE,
-    firstName VARCHAR(50)  NOT NULL,
-    lastName  VARCHAR(50),
-    password  VARCHAR(100) NOT NULL,
-    createdAt DATE         NOT NULL,
-    lastLogin DATE         NOT NULL
-);
+
--- database/migration/000001_init_migration.up.sql
+++ database/migration/000001_init_migration.up.sql
@@ -1,9 +1,66 @@
-CREATE TABLE organizers
-(
-    email     VARCHAR(256) PRIMARY KEY UNIQUE,
-    firstName VARCHAR(50)  NOT NULL,
-    lastName  VARCHAR(50),
-    password  VARCHAR(100) NOT NULL,
-    createdAt DATE         NOT NULL,
-    lastLogin DATE         NOT NULL
+CREATE TABLE
+    Organizers (
+                   uuid UUID PRIMARY KEY,
+                   email VARCHAR(256) UNIQUE,
+                   first_name VARCHAR(50) NOT NULL,
+                   last_name VARCHAR(50),
+                   bio VARCHAR(200),
+                   gender VARCHAR(1),
+                   password VARCHAR(100) NOT NULL,
+                   created_at DATE NOT NULL,
+                   last_login DATE NOT NULL
 );
+
+CREATE TABLE
+    Hackathons (
+                   id BIGSERIAL PRIMARY KEY,
+                   name VARCHAR(30) NOT NULL,
+                   tagline VARCHAR(200) NOT NULL,
+                   description TEXT NOT NULL,
+                   contact_email VARCHAR(256) NOT NULL,
+                   host VARCHAR(100) NOT NULL,
+                   hacking_start TIMESTAMP
+                          WITH
+                              TIME ZONE,
+                   deadline TIMESTAMP
+                          WITH
+                              TIME ZONE,
+                   created_by UUID REFERENCES Organizers(uuid)
+);
+
+CREATE TABLE
+    Participants (
+                     uuid UUID PRIMARY KEY,
+                     email VARCHAR(256) UNIQUE,
+                     first_name VARCHAR(50) NOT NULL,
+                     last_name VARCHAR(50),
+                     bio VARCHAR(200),
+                     gender VARCHAR(1),
+                     password VARCHAR(100) NOT NULL,
+                     created_at DATE NOT NULL,
+                     last_login DATE NOT NULL
+);
+
+CREATE TABLE
+    Projects (
+                 id BIGSERIAL PRIMARY KEY,
+                 name VARCHAR(100) NOT NULL,
+                 description TEXT,
+                 source_code TEXT,
+                 video_link TEXT,
+                 screenshot_link TEXT,
+                 hackathon_id BIGSERIAL REFERENCES Hackathons(id) NOT NULL
+);
+
+CREATE TABLE
+    Teams (
+              team_id UUID PRIMARY KEY,
+              hackathon_id BIGSERIAL REFERENCES Hackathons(id) NOT NULL,
+              name VARCHAR(50) NOT NULL,
+              members TEXT [] NOT NULL,
+              project_id BIGSERIAL REFERENCES Projects(id)
+);
+
+
+SET
+timezone = 'Asia/Calcutta';
